f427cd6c4a562dc181fbd3a04e01e7ec431cd16f
Walie-Matthieu <whoumad@gmail.com>
2024-12-20 00:18:33 +0100

    new packages

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{vue,js,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  theme: {
+    extend: {}, // Personnalise ici si nécessaire
+  },
+  plugins: [], // Ajoute des plugins Tailwind ici si besoin
+};
+
+
